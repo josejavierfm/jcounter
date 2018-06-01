ab5870956bb0f3d455306cfef619081b22c88167
josejavierfm <josejavierfm@gmail.com>
2018-06-01 11:53:17 +0200

    autollamada
    ya no hace falta llamarlo despues, lo hace automaticamente con document.ready

--- jcounter.js
+++ jcounter.js
@@ -1,14 +1,16 @@
 /*
-	jCOUNTER.JS
-	V. 1.0.1
-	José Javier Fernández Mendoza 2018
+	COUNTER.JS
+	V. 1.0.2
+	José Javier Fernández Mendoza 2017
 	Require jquery
 	Añade un parrafo a cada elemento con la clase "cuentacaracteres" para poner el contador de caracteres y los caracteres maximos
 	El parrafo se llamara igual que la caja asociada terminando en _limit y tendrá la clase "limit"
 	Añade el codigo necesario css en el head 
+	1.0.2 ya no es necesario llamarlo despues, lo hace automaticamente en document.ready
+	1.0.1 mejora en maxlength
+	1.0.0 primera version
 	
-	Para llamarlo añadir la linea despues de incluir jquery y este fichero
-	  $('.cuentacaracteres').countChar();
+	
 	
 */
 (function($) {
@@ -20,32 +22,24 @@
       $(element).keyup(function updateCharCounter() {
 
         var $me = $(this);
-        var maxLength = isNaN(parseInt($me.attr('maxlength'))) ? 0 : parseInt($me.attr('maxlength'));
+        var maxLength = parseInt($me.attr('maxlength'), 10);
         var charCount = $me.val().length;
         var nombrecajalimite=$me.prop('id')+"_limit";
         var cajalimite = $('#'+nombrecajalimite);
-        if (maxLength>0){
-          cajalimite.text('' + maxLength + '/' + (maxLength - charCount));
-        }else{
-          cajalimite.text((charCount));
-        }
+
+        cajalimite.text('' + maxLength + '/' + (maxLength - charCount));
       });
 	   
-        var $me = $(this);
-        var maxLength = isNaN(parseInt($me.attr('maxlength'))) ? 0 : parseInt($me.attr('maxlength'));
-        var charCount = $me.val().length;
-		    var nombrecajalimite=$me.prop('id')+"_limit";
-        var cajalimite = $('#'+nombrecajalimite);
-		    if (cajalimite){
-			     $me.after('<p id="'+nombrecajalimite+'" class="limit"></p>');
-		    }
-		    cajalimite = $('#'+nombrecajalimite);
-        if (maxLength>0){
-          cajalimite.text('' + maxLength + '/' + (maxLength - charCount));
-        }else{
-          cajalimite.text((charCount));
-        }
-		    
+        var $me = $(this),
+          maxLength = parseInt($me.attr('maxlength'), 10),
+          charCount = $me.val().length;
+		  var nombrecajalimite=$me.prop('id')+"_limit";
+          var cajalimite = $('#'+nombrecajalimite);
+		 if (cajalimite.length==0){
+			$me.after('<p id="'+nombrecajalimite+'" class="limit"></p>');
+		  }
+		   cajalimite = $('#'+nombrecajalimite);
+		  cajalimite.text('' + maxLength + '/' + (maxLength - charCount));
      
     });
   };
@@ -53,3 +47,6 @@
   $( "<style>.limit {  float: right;  clear: both;  margin-bottom: 10px;  font: 400 0.688em/1.38 'Roboto';  text-align: right;  color: #777777;}</style>" ).appendTo( "head" )
   
 }(jQuery));
+$( document ).ready(function() {
+	 $('.cuentacaracteres').countChar();
+});
